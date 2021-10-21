22abe09908ed5aacbab259b8d1b6332321365734
marcelofi <marce.fernandezi@gmail.com>
2021-10-21 18:52:34 -0400

    Add files for the project

--- cypress/integration/more-commands/cypress-browser.spec.js
+++ cypress/integration/more-commands/cypress-browser.spec.js
@@ -12,5 +12,11 @@ describe('Cypress Browser Demo',()=>{
             cy.log(`State of the dropdown as soon as I load the website: ${Cypress.dom.isVisible($element)}`)
             
         })
+        cy.get('#section1Heading').click();
+
+        cy.get('.collapse').eq(6).then($element=>{
+            cy.log(`State of the dropdown as soon as I click on the card: ${Cypress.dom.isVisible($element)}`)
+            
+        })
     })
 })
